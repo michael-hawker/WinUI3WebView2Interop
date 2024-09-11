9830e3b0c7fed9145430c91c85f822449499dad3
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-09-10 18:45:52 -0700

    Add Latest Available WebView2 package
    Add simple WebView2 to app

--- WebView2HostApp/MainPage.xaml.cs
+++ WebView2HostApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml.Controls;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,5 +14,22 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    private void WebView2_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs args)
+    {
+        if (args.Exception != null)
+        {
+            // We hit an issue with WebView2 load
+            // TODO: Handle this scenario
+            Debugger.Break();
+        }
+    }
+
+    private void WebView2_NavigationCompleted(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs args)
+    {
+        // TODO: Use XAML Binding
+        // Note: This ensures we don't display an empty WebView until our page content has loaded.
+        WebView2.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
     }
 }
