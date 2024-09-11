c8d3bc91c04da86e5b8e6ec9f30b878a949a02e4
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-09-10 23:04:15 -0700

    Port over AddHostObjectToScript sample from UWP with C++/WinRT adapter for wv2winrt and C#/WinRT Bridge Component
    Following https://learn.microsoft.com/en-us/microsoft-edge/webview2/how-to/winrt-from-js?tabs=winui3%2Cwinrtcsharp and adapting UWP sample: https://github.com/MicrosoftEdge/WebView2Samples/blob/main/SampleApps/webview2_sample_uwp/Pages/AddHostObject.xaml
    Uses https://learn.microsoft.com/en-us/windows/apps/develop/platform/csharp-winrt/create-winrt-component-winui-cswinrt to create a C#/WinRT component consumable by both projects, needed additional CsWinRTGeneratedFilesDir to workaround issue 52711016
    Had to add https://learn.microsoft.com/en-us/microsoft-edge/webview2/how-to/winrt-from-js?tabs=winui3%2Cwinrtcsharp#generate-the-api-code workaround for WebView2 winmd reference in WinRTAdapter project - defining WebView2SDKPath, and manually add C#/WinRT project reference
    Needed CsWinRTIncludes to bring C++ projection to C#, and CsWinRTInputs with GenreratePathProperty to get the WebView2 types
    Compiles now, but hits member not found issue when trying to initialize the bridge in AddHostObjectToScript, this maybe worked at one time or regression? Not sure... https://github.com/MicrosoftEdge/WebView2Feedback/issues/2937

---
+++ AddHostObjectBridgeComponent/Bridge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddHostObjectBridgeComponent;
+
+public sealed partial class Bridge
+{
+    private List<string> _items = new List<string>();
+    public event EventHandler<IList<string>> ItemsChangedEvent;
+
+    public Bridge() { }
+
+    public string StringProperty { get; set; } = "example";
+
+    public void ClearItems()
+    {
+        _items.Clear();
+        PostItemsChangedEvent();
+    }
+
+    public void AppendToItems(string item)
+    {
+        _items.Add(item);
+        PostItemsChangedEvent();
+    }
+
+    private void PostItemsChangedEvent()
+    {
+        if (ItemsChangedEvent != null)
+        {
+            ItemsChangedEvent(this, _items);
+        }
+    }
+}
+
--- WebView2HostApp/MainPage.xaml.cs
+++ WebView2HostApp/MainPage.xaml.cs
@@ -1,4 +1,9 @@
+using AddHostObjectBridgeComponent;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -11,9 +16,28 @@ namespace WebView2HostApp;
 /// </summary>
 public sealed partial class MainPage : Page
 {
+    private Bridge _bridge;
+
     public MainPage()
     {
-        this.InitializeComponent();
+        _bridge = new Bridge();
+        _bridge.ItemsChangedEvent += UpdateItemsList;
+
+        InitializeComponent();
+
+        InitializeWebView2Async();
+    }
+
+    private async void InitializeWebView2Async()
+    {
+        await WebView2.EnsureCoreWebView2Async();
+
+        WebView2.CoreWebView2.Settings.AreHostObjectsAllowed = true;
+        WebView2.CoreWebView2.SetVirtualHostNameToFolderMapping("appassets.html.example", "html", CoreWebView2HostResourceAccessKind.Allow);
+        WebView2.Source = new Uri("http://appassets.html.example/add_host_object.html");
+
+        var dispatchAdapter = new WinRTAdapter.DispatchAdapter();
+        WebView2.CoreWebView2.AddHostObjectToScript("BridgeInstance", dispatchAdapter.WrapObject(_bridge, dispatchAdapter));
     }
 
     private void WebView2_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs args)
@@ -31,5 +55,21 @@ public sealed partial class MainPage : Page
         // TODO: Use XAML Binding
         // Note: This ensures we don't display an empty WebView until our page content has loaded.
         WebView2.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
+    }
+
+    private void UpdateItemsList(object sender, IList<string> items)
+    {
+        ItemsListTextBox.Text = string.Join("\n", items);
+    }
+
+    private void ItemButton_Click(object sender, RoutedEventArgs e)
+    {
+        var text = (sender as Button).Content as string;
+        _bridge.AppendToItems(text);
+    }
+
+    private void ClearButton_Click(object sender, RoutedEventArgs e)
+    {
+        _bridge.ClearItems();
     }
 }
